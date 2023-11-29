2e39ac7182d933f3617feda8e38f0479e28ab4a3
enkog <nkirukaogu@gmail.com>
2023-11-29 20:16:42 +0100

    Implement testimonial section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { urlFor, client } from "../../client";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 
 import "./About.scss";
 
@@ -15,7 +15,7 @@ const About = () => {
   }, []);
 
   return (
-    <div className="app__about">
+    <div>
       <h2 className="head-text">
         I Know That <span>Good Apps</span>
         <br />
@@ -44,4 +44,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, "about");
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { client, urlFor } from "../../client";
 
 import "./Work.scss";
-import { client, urlFor } from "../../client";
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState("All");
@@ -116,4 +116,8 @@ const Work = () => {
   );
 };
 
-export default AppWrap(Work, "work");
+export default AppWrap(
+  MotionWrap(Work, "app__works"),
+  "work",
+  "app__primarybg"
+);
