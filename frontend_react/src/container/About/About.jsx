255fb04f24b1fc9a73071faf04a9ccb6fbee9bba
enkog <nkirukaogu@gmail.com>
2023-11-29 15:30:35 +0100

    Connect sanity to frontend, Implement About section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,33 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
+import { urlFor, client } from "../../client";
 
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good web developer",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Frontend Development",
-    description: "I am a good web developer",
-    imgUrl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I am a good web developer",
-    imgUrl: images.about03,
-  },
-  {
-    title: "MERN Stack",
-    description: "I am a good web developer",
-    imgUrl: images.about04,
-  },
-];
-
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <div className="app__about">
       <h2 className="head-text">
@@ -44,7 +29,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
