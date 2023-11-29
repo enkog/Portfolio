8efc04b8bcd2d41e56285cf0d4d7f9ef01317a76
enkog <nkirukaogu@gmail.com>
2023-11-29 16:14:08 +0100

    Implement socials, navigationdots and app wrapper

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { urlFor, client } from "../../client";
+import { AppWrap } from "../../wrapper";
 
 import "./About.scss";
 
@@ -43,4 +44,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
